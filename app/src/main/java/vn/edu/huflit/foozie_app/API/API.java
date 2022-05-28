6fb03c0635fe3760f201834b15941bf57cb8f442
vinhphan812 <vinhphan812@gmail.com>
2022-05-28 23:02:45 +0700

    sync BE

--- app/src/main/java/vn/edu/huflit/foozie_app/API/API.java
+++ app/src/main/java/vn/edu/huflit/foozie_app/API/API.java
@@ -6,11 +6,13 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.IOException;
 import java.lang.reflect.Type;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
