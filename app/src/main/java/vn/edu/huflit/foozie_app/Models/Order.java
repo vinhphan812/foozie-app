ee29569767d73e353c487dc0aee444cf44b68046
vinhphan812 <vinhphan812@gmail.com>
2022-06-22 17:21:22 +0700

    sync

--- app/src/main/java/vn/edu/huflit/foozie_app/Models/Order.java
+++ app/src/main/java/vn/edu/huflit/foozie_app/Models/Order.java
@@ -1,7 +1,5 @@
 package vn.edu.huflit.foozie_app.Models;
 
-import android.location.Address;
-
 import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
@@ -12,8 +10,6 @@ public class Order extends BaseDTO {
     public int total_foods;
     @SerializedName("order_date")
     public Date order_date;
-    @SerializedName("voucher_ship")
-    public String voucher_ship;
     @SerializedName("voucher_using")
     public String voucher_using;
     @SerializedName("delivery")
@@ -30,4 +26,6 @@ public class Order extends BaseDTO {
     public int shipping_fee;
     @SerializedName("total")
     public int total;
+    @SerializedName("details")
+    public List<Food> details;
 }
